4a1bf38406bea65866ae1a0133f4f4f6ff15f66e
Guillermo Ruiz Álvarez <guillermo.ruiz.alv@gmail.com>
2019-06-22 21:40:48 +0100

    Add FSRCNN

--- models.py
+++ models.py
@@ -1,8 +1,89 @@
 import tensorflow as tf
 from tensorflow.keras import layers
 
-def IRCNN(input_shape):
+def FSRCNN(input_shape, upscale):
+    '''FSRCNN model
+    :param input_shape: 3-tuple representing the image shape
+    :param upscale: upscale factor
+    '''
 
+    # Attributes
+    d = 48 # LR feature dimension
+    s = 16 # Number of shrinking filters
+    m = 4 # Mapping depth
+
+    # Initializers
+    w_init = tf.keras.initializers.VarianceScaling(seed=0)
+    w_deconv_init = tf.keras.initializers.RandomNormal(mean=0.0, stddev=0.001, seed=0)
+
+    # Create model
+    model = tf.keras.models.Sequential()
+
+    # Feature extraction
+    model.add(layers.Conv2D(
+        input_shape = input_shape,
+        filters = d,
+        kernel_size = 5,
+        padding = "same",
+        data_format = "channels_last",
+        activation = layers.PReLU,
+        use_bias = True,
+        kernel_initializer = w_init,
+    ))
+
+    # Shrinking
+    model.add(layers.Conv2D(
+        filters = s,
+        kernel_size = 1,
+        padding = "same",
+        data_format = "channels_last",
+        activation = layers.PReLU,
+        use_bias = True,
+        kernel_initializer = w_init,
+    ))
+
+    # Mapping
+    for _ in range(m):
+        model.add(layers.Conv2D(
+            filters = s,
+            kernel_size = 3,
+            padding = "same",
+            data_format = "channels_last",
+            activation = layers.PReLU,
+            use_bias = True,
+            kernel_initializer = w_init,
+        ))
+
+    # Expanding
+    model.add(layers.Conv2D(
+        filters = d,
+        kernel_size = 1,
+        padding = "same",
+        data_format = "channels_last",
+        activation = layers.PReLU,
+        use_bias = True,
+        kernel_initializer = w_init,
+    ))
+
+    # Deconvolution
+    model.add(layers.Conv2DTranspose(
+        filters = 1
+        kernel_size = 9
+        strides = upscale,
+        padding = "same",
+        data_format = "channels_last",
+        activation = None,
+        use_bias = True,
+        kernel_initializer = w_deconv_init,
+    ))
+
+    return model
+
+
+def IRCNN(input_shape):
+    '''IRCNN model
+    :param input_shape: 3-tuple representing the image shape
+    '''
     # Attributes
     seed = 0
     kernel_weight_decay = 1e-4
